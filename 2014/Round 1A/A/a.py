55cf0839bc38660fee569b41cc626db0219b2b19
cnelson <cnelson@cnelson-ubuntu.(none)>
2014-04-26 14:37:42 -0500

    Round 1A 2014

---
+++ 2014/Round 1A/A/a.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# cat testset.in | ./main.py > testset.out
+
+from __future__ import print_function
+import sys
+
+
+def debug(*objs):
+    print("DEBUG: ", *objs, file=sys.stderr)
+    pass
+
+
+def main():
+    t = int(sys.stdin.readline())
+    for c in range(1, t + 1):
+        (n, l) = map(long, tuple(sys.stdin.readline().split()))
+        outlets = map(lambda x: long(x, 2), sys.stdin.readline().split())
+        devices = map(lambda x: long(x, 2), sys.stdin.readline().split())
+        outlets.sort()
+        devices.sort()
+
+        min_flips = l + 1
+        for i in range(0, n):
+            for j in range(0, n):
+                candidate = outlets[i] ^ devices[j]
+                num_ones = bin(candidate).count("1")
+                if num_ones < min_flips:
+                    temp_outlets = map(lambda x: x ^ candidate, outlets)
+                    temp_outlets.sort()
+                    if devices == temp_outlets:
+                        min_flips = num_ones
+
+        if min_flips <= l:
+            print("Case #%i: %i" % (c, min_flips))
+        else:
+            print("Case #%i: NOT POSSIBLE" % c)
+
+
+if __name__ == "__main__":
+    main()
